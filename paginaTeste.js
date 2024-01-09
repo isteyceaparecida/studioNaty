9f6a6a1cc5d3514f12167ae8e7e981af8b80743e
isteyceaparecida <156094901+isteyceaparecida@users.noreply.github.com>
2024-01-09 18:58:40 -0300

    Add files via upload

---
+++ paginaTeste.js
@@ -0,0 +1,7 @@
+function clica(){
+    alert("Conheça um pouco mais sobre a gente" + "!")
+}
+function MeuContato(){
+    var contato = document.getElementById("contato")
+    contato.style.backgroundColor = " #941bc4"
+}
